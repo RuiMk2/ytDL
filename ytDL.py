8921f31f68a0b31123298ea179d65d094f898786
RuiMk2 <109416949+RuiMk2@users.noreply.github.com>
2024-08-07 00:21:15 +0800

    initial commit

---
+++ ytDL.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+from moviepy.editor import VideoFileClip, AudioFileClip
+import sys
+import os
+videoURL = ""
+if (len(sys.argv) > 1):
+    videoURL = sys.argv[1]
+if ("youtube.com" not in videoURL):
+    videoURL = input("Enter YouTube URL: ")
+yt = YouTube(videoURL,use_oauth=True,allow_oauth_cache=True)
+filename = yt.title.replace(" ","_")
+print("Downloading YouTube File: " + yt.title)
+yt.streams.order_by("resolution").desc().first().download(filename=filename + "v.mp4")
+yt.streams.filter(only_audio=True).first().download(filename=filename + "a.mp3")
+
+video = VideoFileClip(filename + "v.mp4")
+audio = AudioFileClip(filename + "a.mp3")
+
+output = video.set_audio(audio)
+output.write_videofile(filename + ".mp4")
+os.remove(filename + "v.mp4")
+os.remove(filename + "a.mp3")
