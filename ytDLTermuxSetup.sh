391e08c1550f39b1aebccf3c2fd2b13b4a3c4b86
RuiMk2 <109416949+RuiMk2@users.noreply.github.com>
2024-08-07 02:26:04 +0800

    Update ytDLTermuxSetup.sh

--- ytDLTermuxSetup.sh
+++ ytDLTermuxSetup.sh
@@ -7,4 +7,4 @@ pkg install ffmpeg
 pip install pytube
 pip install moviepy
 
-cp cipher.py /data/data/com.termux/files/usr/lib/python3.11/site-packages/pytube.cipher.py
+cp cipher.py /data/data/com.termux/files/usr/lib/python3.11/site-packages/pytube/cipher.py
